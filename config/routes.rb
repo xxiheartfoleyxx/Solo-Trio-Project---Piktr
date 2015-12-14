ad5568d7e7b6a5bad9cb7ca56326d7f855822348
Stefani <eatatjoez8@aol.com>
2015-12-14 10:33:17 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
-  get 'signup'  =>  'users/new'
+  get 'signup'  =>  'users#new'
 
   resources :pictures
   # The priority is based upon order of creation: first created -> highest priority.
